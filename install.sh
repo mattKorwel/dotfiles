5da76087ecb7fac4b9deef2fcb2fdca56a480578
matt korwel <mattkorwel@github.com>
2021-06-14 15:37:00 -0700

    again

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt install fonts-firacode python3-dev python3-pip python3-setuptools zsh-syntax-highlighting  -y
+sudo apt install fonts-firacode python3-dev python3-pip python3-setuptools -y
 sudo pip3 install thefuck
 
 # git config
@@ -14,11 +14,16 @@ git config --global remote.origin.prune true
 git config --global pull.rebase true
 git config --global github.user "mattKorwel"
 git config --global push.default "simple"
-git config --global url.git@github.com:.insteadof=https://github.com/
+git config --global url."git@github.com:".insteadOf https://github.com/
 git config --global commit.gpgsign true
 
 # git completion
 curl -fLo ~/.zsh/git-completion.zsh --create-dirs https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+
+#kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 
 # links
 ln -s -f ~/.dotfiles/.zshrc ~/
@@ -27,6 +32,12 @@ mkdir -p ~/.config && ls -s -f ~/.dotfiles/starship.toml ~/.config
 # zsh 
 if [ "$SHELL" != "/usr/bin/zsh" ]; then
     sudo apt install -y zsh
-    sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --platform unknown-linux-musl
-    sh -c " KEEP_ZSHRC=yes ZSH=$HOME/.oh-my-zsh $(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi;
+    sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --platform unknown-linux-musl --yes
+    sh -c " KEEP_ZSHRC=yes CHSH=no RUNZSH=no ZSH=$HOME/.oh-my-zsh $(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    ZSH_AS="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+    rm -rf ${ZSH_AS} git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_AS}
+
+    ZSH_SH="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    rm -rf ${ZSH_SH} && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_SH}
+fi
